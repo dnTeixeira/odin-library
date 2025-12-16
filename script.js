81bee238203385dedf9ef64322cc3b5c5940da5c
Daniel Teixeira <danielteixeira33@hotmail.com>
2025-12-16 17:10:05 -0300

    feat: add book constructor and addBookToLibrary function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if(!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
