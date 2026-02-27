f58f61949e1a8e8920e6e9475f87422be0ae23bd
Daniel Teixeira <danielteixeira33@hotmail.com>
2026-02-27 20:17:31 -0300

    feat: add modal dialog to create new books

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 const myLibrary = [];
 
+const closeDialog = document.querySelector("#close-btn");
+const newBookButton = document.querySelector("#new-book-btn");
+const newBookDialog = document.querySelector("#new-book-dialog");
+const newBookForm = document.querySelector("#new-book-form")
+
+const confirmButton = document.querySelector("#confirm-btn");
+
+newBookButton.addEventListener("click", () => {
+    newBookDialog.showModal();
+})
+
+closeDialog.addEventListener("click", () => {
+    newBookDialog.close();
+})
+
+confirmButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let title = document.querySelector("#book-title").value;
+    let author = document.querySelector("#book-author").value;
+    let pages = document.querySelector("#book-pages").value;
+    let read = "read";
+    
+    addBookToLibrary(title, author, pages, read);
+
+    newBookForm.reset();
+    newBookDialog.close();
+
+    title = "";
+    author = "";
+    pages = null;
+    read = null;
+})
+
 function Book(title, author, pages, read) {
     if(!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
@@ -17,10 +51,16 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
+    displayBooks();
 }
 
 function displayBooks() {
     const booksGrid = document.querySelector(".books");
+    const existingBooks = booksGrid.querySelectorAll(".book-card");
+
+    existingBooks.forEach((existingBook) => {
+        existingBook.remove();
+    });
 
     myLibrary.forEach((book) => {
         const bookCard = document.createElement("div");
@@ -38,5 +78,3 @@ function displayBooks() {
         booksGrid.appendChild(bookCard);
     });
 }
-
-displayBooks();
